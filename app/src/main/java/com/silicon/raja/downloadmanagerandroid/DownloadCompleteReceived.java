93ee536037dc9dfccd38c24e918e064226b871ef
Raja Jawahar <rajajawahar77@gmail.com>
2017-03-30 16:40:27 +0530

    Notification added with PendingIntent

--- app/src/main/java/com/silicon/raja/downloadmanagerandroid/DownloadCompleteReceived.java
+++ app/src/main/java/com/silicon/raja/downloadmanagerandroid/DownloadCompleteReceived.java
@@ -1,6 +1,9 @@
 package com.silicon.raja.downloadmanagerandroid;
 
 import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -8,11 +11,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by rajamohamed on 29/03/17.
@@ -48,14 +53,39 @@ public class DownloadCompleteReceived extends BroadcastReceiver {
         final Uri uri = FileProvider.getUriForFile(context, BuildConfig.FILE_PROVIDER_AUTHORITY,
             new File(context.getCacheDir(),
                 BuildConfig.ATTACHMENT_DOWNLOAD_CACHE_FOLDER + "/" + dstFile.getName()));
-        context.getSharedPreferences("AttachmentPreferences", Context.MODE_PRIVATE)
+        context.getSharedPreferences("AttachmeztPreferences", Context.MODE_PRIVATE)
             .edit()
             .putString(fileUrl + "_file_path", dstFile.getPath())
             .commit();
+        openHeadsUpNotification(context, uri, dstFile.getName());
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
     cursor.close();
+  }
+
+  // Add Notification once the download completed
+  private void openHeadsUpNotification(Context context, Uri uri, String fileName) {
+    Random random = new Random();
+    int id = random.nextInt(1000);
+
+    Intent openViewIntent = new Intent(Intent.ACTION_VIEW);
+    openViewIntent.setDataAndType(uri, context.getContentResolver().getType(uri));
+    openViewIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    openViewIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    PendingIntent piView =
+        PendingIntent.getActivity(context, id, openViewIntent, PendingIntent.FLAG_ONE_SHOT);
+
+    NotificationCompat.Builder builder =
+        new NotificationCompat.Builder(context).setContentTitle(fileName)
+            .setContentIntent(piView)
+            .setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+    NotificationManager notificationManager =
+        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    notificationManager.notify(id, builder.build());
   }
 }
